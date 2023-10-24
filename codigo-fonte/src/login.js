c67d7812c1a9bd4f2419cde8bd95e1f0e4a4f46b
NathanLuizz <144962568+NathanLuizz@users.noreply.github.com>
2023-10-23 21:38:47 -0300

    teste

---
+++ codigo-fonte/src/login.js
@@ -0,0 +1,11 @@
+document.querySelector('.img-btn').addEventListener('click', function()
+	{
+		document.querySelector('.cont').classList.toggle('s-signup')
+	}
+    
+    
+);
+const btn=document.querySelector('button');
+btn.addEventListener('click',function(){
+    alert("Bem vindo");
+});
